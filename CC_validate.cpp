a1742972075d807482128498882b0676ee41ae23
Thirumalai <thirumalaicse2020@gmail.com>
2023-12-24 12:24:58 +0530

    main code

--- CC_validate.cpp
+++ CC_validate.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;    
+
+
+int main()  
+{  
+    long int number;
+    cout << "Enter a credit card number: ";
+    cin >> number;
+    cout << number << " is " << (isValid(number) ? "valid" : "invalid");  
+    return 0;  
+}  
