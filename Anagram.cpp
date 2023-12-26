175c1e597d18e620a90d96891fa2ce410312b826
Thirumalai Nagalingam <81082960+thiru31@users.noreply.github.com>
2023-12-26 14:00:23 +0530

    Updating main Function ofAnagram.cpp

--- Anagram.cpp
+++ Anagram.cpp
@@ -1,8 +1,28 @@
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
 
-int main()
+bool areAnagram(string str1, string str2) 
+
 {
-  return 0;
+
 }
 
+int main() 
+
+{
+
+    string str1; 
+    string str2;
+    cin>>str1>>str2;
+
+    if (areAnagram(str1, str2)) 
+
+        cout << "The two strings are anagram"; 
+
+    else 
+
+        cout << "The two strings are not anagram "; 
+
+    return 0; 
+
+}
