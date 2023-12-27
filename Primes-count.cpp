ca57d6836db4671861cca4ef0a4f11cd2b814e41
Thirumalai Nagalingam <81082960+thiru31@users.noreply.github.com>
2023-12-27 20:22:51 +0530

    Initialization of code

---
+++ Primes-count.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPrimes(int strt,int end)
+{
+   
+   }
+   return count;
+}
+int main(){
+   int START, END;
+   cin>>START>>END;
+   cout <<endl<<"Primes in Ranges : "<<countPrimes(START,END);
+   return 0;
+}
