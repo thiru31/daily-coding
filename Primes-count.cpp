395adc425e57d747edf20f570faed2d899c132f2
Thirumalai Nagalingam <81082960+thiru31@users.noreply.github.com>
2023-12-27 20:27:38 +0530

    Update Primes-count.cpp

--- Primes-count.cpp
+++ Primes-count.cpp
@@ -1,9 +1,19 @@
 #include <bits/stdc++.h>
-using namespace std;
-
-int countPrimes(int strt,int end)
-{
-   
+using namespace std; 
+int isprime(int num){
+   if (num <= 1)
+      return 0;
+   for (int i = 2; i <= num/2; i++){
+      if (num % i == 0)
+         { return 0; }
+   }
+   return 1;
+}
+int countPrimes(int strt,int end){
+   int count=0;
+   for(int i=strt;i<=end;i++){
+      if(isprime(i)==1)
+         { count++; }
    }
    return count;
 }
