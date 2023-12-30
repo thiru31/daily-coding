65992ddc6f4ed94121139139710a9929e748062f
Thirumalai <thirumalaicse2020@gmail.com>
2023-12-30 12:31:04 +0530

    CC validate final commit

--- CC_validate.cpp
+++ CC_validate.cpp
@@ -1,24 +1,33 @@
-#include <iostream>
-using namespace std;    
-
-int sumOfDoubleEvenPlace(long int number)  
-{  
-    int sum = 0;  
-    while (number != 0)  
-    {  
-        number = number / 10;  
-        sum = sum + getDigit((number % 10) * 2);  
-        number = number / 10;  
-    }  
-    return sum;  
+#include <bits/stdc++.h>
+using namespace std;
+ 
+bool checkLuhn(const string& cardNo)
+{
+    int nDigits = cardNo.length();
+ 
+    int nSum = 0, isSecond = false;
+    for (int i = nDigits - 1; i >= 0; i--) {
+ 
+        int d = cardNo[i] - '0';
+ 
+        if (isSecond == true)
+            d = d * 2;
+        nSum += d / 10;
+        nSum += d % 10;
+ 
+        isSecond = !isSecond;
+    }
+    return (nSum % 10 == 0);
 }
-
-
-int main()  
-{  
-    long int number;
-    cout << "Enter a credit card number: ";
-    cin >> number;
-    cout << number << " is " << (isValid(number) ? "valid" : "invalid");  
-    return 0;  
-}  
+ 
+int main()
+{
+    cout<<"Enter card number:";
+    string cardNo;
+    cin>>cardNo;
+    if (checkLuhn(cardNo))
+        printf("This is a valid card");
+    else
+        printf("This is not a valid card");
+    return 0;
+}
