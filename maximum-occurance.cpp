e541cd028ef48800af0ec8eae7dc0ea74637e4f7
Thirumalai <thirumalaicse2020@gmail.com>
2024-01-01 20:24:00 +0530

    maximum occurace final

--- maximum-occurance.cpp
+++ maximum-occurance.cpp
@@ -1,9 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Given a string(lowercase), find the maximum occurance of a character in the string. 
+
 int main(){
 
     string s;
     cin>>s;
+    int n = s.length();
+    int count[26] = {0};
+    for(int i=0;i<n;i++){
+        count[s[i]-'a']++;
+    }
+    int max = 0;
+    char result;
+    for(int i=0;i<26;i++){
+        if(count[i]>max){
+            max = count[i];
+            result = i+'a';
+        }
+    }
+    cout<<result<<" "<<max<<endl;
+
     return 0;
 }
