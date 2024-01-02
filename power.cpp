dcec5a3d9cddd07dee4ed0c3df2dea9a344e1a2d
Thirumalai <thirumalaicse2020@gmail.com>
2024-01-02 19:30:10 +0530

    fetching input in power.cpp

--- power.cpp
+++ power.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
+    int base, power, i, pow = 1;
+    cout << "Enter base of a number :: ";
+    cin >> base;
+    cout << "\nEnter power to a base <<base>> ::";
+    cin >> power;
+
 
     return 0;
 }
