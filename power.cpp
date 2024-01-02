7a1e7d6628a55524570f0d9d3e8c6d590e0fa938
Thirumalai <thirumalaicse2020@gmail.com>
2024-01-02 19:36:31 +0530

    final power.cpp

--- power.cpp
+++ power.cpp
@@ -4,11 +4,15 @@ using namespace std;
 int main()
 {
     int base, power, i, pow = 1;
-    cout << "Enter base of a number :: ";
+    cout << "Enter base of a number : ";
     cin >> base;
-    cout << "\nEnter power to a base <<base>> ::";
+    cout << "\nEnter power to a base "<<base<<": ";
     cin >> power;
 
-
+for(i=power;i>0;i--)
+   {
+        pow=pow*base;
+   }
+   cout<<" \nThe final result calculated is: "<<pow<<endl;
     return 0;
 }
