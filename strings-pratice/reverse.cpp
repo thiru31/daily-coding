daa9a18ad96b61a06ef44b7c73bbf35de23361e4
Thirumalai <thirumalaicse2020@gmail.com>
2024-01-09 00:30:29 +0530

    Added string-reverse.cpp

--- strings-pratice/reverse.cpp
+++ strings-pratice/reverse.cpp
@@ -5,5 +5,10 @@ using namespace std;
 
 int main()
 {
+    string brev, arev;
+    cout << "Enter the string to reverse :";
+    cin >> brev;
+    arev = string(brev.rbegin(), brev.rend());
+    cout << "Reversed string is :" << arev << endl;
     return 0;
 }
