71e11984dc074a9317b4cf3a3a7607da688cd947
Thirumalai <thirumalaicse2020@gmail.com>
2024-01-11 02:59:38 +0530

    Add num-triangle.cpp to simple-patterns directory

---
+++ simple-patterns/num-triangle.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = i; j <= rows; j++)
+        {
+            cout << " " << j;
+        }
+        for (int j = 1; j <= i - 1; j++)
+        {
+            cout << " " << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
