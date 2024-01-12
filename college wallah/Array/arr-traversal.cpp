030bce8a5548d58ef8db910a67fccf48de52c44f
Thirumalai <thirumalaicse2020@gmail.com>
2024-01-13 00:47:02 +0530

    Update array traversal to allow user input (using for loop)

--- college wallah/Array/arr-traversal.cpp
+++ college wallah/Array/arr-traversal.cpp
@@ -5,7 +5,16 @@ using namespace std;
 int main()
 {
     int size;
-    int arr[] = {1, 2, 3, 4, 5};
+    // int arr[] = {1, 2, 3, 4, 5};
+    cout<<"Enter the size of array :";
+    cin>>size;
+    int arr[size];
+    cout<<"Enter the Elements of array :"<<endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin>>arr[i];
+    }
+
     // size = end(arr)-begin(arr);
     size = sizeof(arr) / sizeof(arr[0]);
     for (int i = 0; i < size; i++)
