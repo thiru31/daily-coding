86a2e5053cf2d306c1bafe88669ee23dff45bded
Thirumalai <thirumalaicse2020@gmail.com>
2024-01-18 01:21:05 +0530

    Add power of 2 program using bit manipulation

---
+++ college wallah/bit-manip/power-of-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int k;
+    cout<<"Enter num to check pow of 2 :";
+    cin >> k;
+    if (k & (k - 1))
+    {
+        cout << "Not power of 2";
+    }
+    else
+        cout << "power of 2";
+}
