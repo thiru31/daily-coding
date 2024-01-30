3ebb8403f69936081116c32db8a2ab1ba0cd7df7
Thirumalai <thirumalaicse2020@gmail.com>
2024-01-30 13:08:33 +0530

    Add code to check if two linked lists are equal

---
+++ college wallah/Linkedlist/check-2ll-areequal.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int val;
+    node *next;
+    node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+class Linkedlist
+{
+public:
+    node *head;
+    Linkedlist()
+    {
+        head = NULL;
+    }
+
+    void insert(int val)
+    {
+        node *newnode = new node(val);
+        node *temp = head;
+        if (temp == NULL)
+        {
+            head = newnode;
+            return;
+        }
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+        newnode->next = NULL;
+    }
+    void display()
+    {
+        node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->val << "-->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+void comp_nodes(node *head1, node *head2)
+{
+    int flag = 1;
+    while (head1 != NULL && head2 != NULL)
+        if (head1->val == head2->val)
+        {
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+        else
+        {
+            flag = 0;
+            break;
+        }
+    if (head1 != NULL || head2 != NULL)
+    {
+        flag = 0;
+    }
+
+    flag == 1 ? cout << "Same" : cout << "not same";
+}
+
+int main()
+{
+    Linkedlist list1;
+    Linkedlist list2;
+    for (size_t i = 1; i < 5; i++)
+    {
+        list1.insert(i);
+        list2.insert(i);
+    }
+    list1.insert(6);
+    list1.insert(6);
+    list2.insert(6);
+    list1.display();
+    list2.display();
+    comp_nodes(list1.head, list2.head);
+}
