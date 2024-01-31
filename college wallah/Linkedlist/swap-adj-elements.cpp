2feac536da40c3690058dce8d12a04a4a380cac4
Thirumalai <thirumalaicse2020@gmail.com>
2024-01-31 16:56:38 +0530

    Add code for swapping adjacent elements in a linked list

---
+++ college wallah/Linkedlist/swap-adj-elements.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int val;
+    node *next;
+
+    node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+class linkedlist
+{
+public:
+    node *head;
+    linkedlist()
+    {
+        head = NULL;
+    }
+    void insert(int val)
+    {
+        node *temp = head;
+        node *newnode = new node(val);
+        if (temp == NULL)
+        {
+            head = newnode;
+            return;
+        }
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+        newnode->next = NULL;
+    }
+    void display()
+    {
+        node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->val << "-->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+node *reverse_k_nodes(node *&head, int k)
+{
+    node *prev = NULL;
+    node *curr = head;
+    int count = 0;
+    while (curr != NULL && count < k)
+    {
+        node *after = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = after;
+        count++;
+    }
+    if (curr != NULL)
+    {
+        node *newhead = reverse_k_nodes(curr, k);
+        head->next = newhead;
+    }
+    return prev;
+}
+
+int main()
+{
+    int k = 2;
+    // cout << "Enter the Number of nodes to be reversed at a time :";
+    // cin >> k;
+    linkedlist ll;
+    ll.insert(10);
+    ll.insert(20);
+    ll.insert(30);
+    ll.insert(40);
+    ll.insert(50);
+    ll.insert(60);
+    ll.insert(70);
+    ll.display();
+    ll.head = reverse_k_nodes(ll.head, k);
+    ll.display();
+}
