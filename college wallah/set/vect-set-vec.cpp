31133a42d3159425622aa78cf7f95f68148291c1
Thirumalai <thirumalaicse2020@gmail.com>
2024-03-30 23:54:08 +0530

    Refactor vector to set and set to vector conversion

--- college wallah/set/vect-set-vec.cpp
+++ college wallah/set/vect-set-vec.cpp
@@ -7,7 +7,9 @@ int main()
 {
     vector<int> arr{1, 2, 1, 2, 3, 4, 5, 5, 6};
     int size = arr.size();
+    // vector to set
     set<int> s(arr.begin(), arr.end());
+    // set to vector
     arr.assign(s.begin(), s.end());
 
     for (int ele : arr)
