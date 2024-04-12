b64278bf89eb2bdfc152bea7dc91607055034124
Thirumalai <thirumalaicse2020@gmail.com>
2024-04-12 23:52:35 +0530

    Add tree.cpp file with code to implement a binary search tree

---
+++ 31-03/tree.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int val;
+    node *left;
+    node *right;
+    node(int data)
+    {
+        val = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class bst
+{
+public:
+    node *root;
+    bst()
+    {
+        root = NULL;
+    }
+    void insert(int val)
+    {
+        root = insertUtil(root, val);
+    }
+    node *insertUtil(node *root, int val)
+    {
+        if (root == NULL)
+        {
+            return new node(val);
+        }
+        if (val > root->val)
+        {
+            root->right = insertUtil(root->right, val);
+        }
+        else if (val < root->val)
+        {
+            root->left = insertUtil(root->left, val);
+        }
+        return root;
+    }
+    void preorder(node *root)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+        cout << root->val << "--";
+        preorder(root->left);
+        preorder(root->right);
+    }
+    void postorder(node *root)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->val << "--";
+    }
+    void inorder(node *root)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+        inorder(root->left);
+        cout << root->val << "--";
+        inorder(root->right);
+    }
+};
+
+int main()
+{
+    bst b;
+    node *root = NULL;
+    b.insert(5);
+    b.insert(34);
+    b.insert(33);
+    b.insert(6);
+    b.insert(7);
+    b.preorder(b.root);
+    cout<<endl;
+    b.postorder(b.root);
+    cout<<endl;
+    b.inorder(b.root);
+    return 0;
+}
